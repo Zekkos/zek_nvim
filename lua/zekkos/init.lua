afe4cfd4ff6a1baebc8bbf758682d201f0cee7b3
Zekkos <dullcraft@gmail.com>
2024-11-24 14:00:18 +1300

    Fixing order of loading

--- lua/zekkos/init.lua
+++ lua/zekkos/init.lua
@@ -1,7 +1,7 @@
+require("zekkos.packer")
 require("zekkos.remap")
 print("Hello from the Zek")
 
-require("zekkos.packer")
 
 vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
