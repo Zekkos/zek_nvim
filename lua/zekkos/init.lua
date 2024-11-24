66a113e67e584e4ac310af8cff755afb381d8bdb
Zekkos <dullcraft@gmail.com>
2024-11-24 13:56:44 +1300

    Actually loading packer properly on launch

--- lua/zekkos/init.lua
+++ lua/zekkos/init.lua
@@ -1,6 +1,8 @@
 require("zekkos.remap")
 print("Hello from the Zek")
 
+require("zekkos.packer")
+
 vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
@@ -8,3 +10,4 @@ vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
 vim.wo.number = true
 vim.wo.relativenumber = true
+
